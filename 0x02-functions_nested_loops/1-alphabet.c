14e8bce93fae6d1bbdb0d996d8cc7c54a5448ef8
zackoder <besszakaria@gmail.com>
2023-03-21 16:02:47 -0700

    commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * main - prints the alphabets 
+ * 
+ *returncs 0
+ * */
+int main(void)
+{
+	int i;
+	for  (i = 'a' ; i < 'z'; i++)
+	       _putchar(i + '0');
+	_putchar('\n');
+}	
