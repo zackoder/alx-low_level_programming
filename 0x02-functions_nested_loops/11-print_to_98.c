f3dfa479a69238ed19177406759288ad6935d116
zackoder <besszakaria@gmail.com>
2023-03-22 16:12:05 -0700

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,21 +12,23 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d, ", n++);
 		}
-		printf("\n");
+		printf("98\n");
 	}
 	else if (n == 98)
+	{
 		printf("%d\n", n);
+	}
 	else
 	{
-		while (0 >= 98)
+		while (n  > 98)
 		{
 			printf("%d, ", n--);
 		}
-		printf("\n");
+		printf("98\n");
 	}
 }
 
