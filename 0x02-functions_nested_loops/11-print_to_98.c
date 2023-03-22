6ec014879c6dcac2f3ec72c9ad75e89f900a6365
zackoder <besszakaria@gmail.com>
2023-03-22 15:44:45 -0700

    commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the num 98 if the num equals 98 
+ * and it count down if the num is greter than 98
+ * and it incres the value to 98 if the num is less than 98
+ * returns (0)
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("\n");
+	}
+	else if (n == 98)
+		printf("%d\n", n);
+	else
+	{
+		while (0 >= 98)
+		{
+			printf("%d, ", n--);
+		}
+		printf("\n");
+	}
+}
+
