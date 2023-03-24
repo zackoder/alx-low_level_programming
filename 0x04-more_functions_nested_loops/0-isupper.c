145beda8c208130e0d72d24f55135327e149951e
zackoder <besszakaria@gmail.com>
2023-03-24 10:28:52 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - that is the _isupper function
+ * it returns 0
+ */
+
+int _isupper(int c)
+{
+	while (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	while (c >= 97 && c >= 122)
+	{
+		return (0);
+	}
+	return (0);
+}
