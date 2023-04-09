6287aec75598a936bc4998cc6a3b0b6d9c5298fb
zackoder <besszakaria@gmail.com>
2023-04-09 14:25:24 -0700

    commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	int a, b, result;
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return 1;
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	result = a * b;
+	printf("%d\n", result);
+	return 0;
+}
+
