e701e07bdffe2f0b481f70dd93b93287faf14042
zackoder <besszakaria@gmail.com>
2023-04-11 15:01:10 -0700

    commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,10 +6,12 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *arr = (char*) malloc(size * sizeof(char));
-	if (size == 0)
-		return NULL;
 	for (i = 0; i < size; i++)
 	{
+		if (arr == NULL)
+		{
+			return NULL;
+		}
 		arr[i] = c;
 	}
 	return (arr);
